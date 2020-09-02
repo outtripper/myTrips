27200e4944b28d743f83ff9a5ad3a13679d9077b
leonardoleenen <leonardo@nexttrip.to>
2020-09-02 20:45:23 +0000

    Adding Trip Card

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react' 
 import {getActiveTrips} from '../services/business'
 import Loading from '../components/Loading'
+import {Card} from 'antd'
 
+const {Meta} = Card
 
 export default ()=> {
-  const [listActiveTrips,setListActiveTrips] = useState(null)
+  const [listActiveTrips,setListActiveTrips] = useState<Array<MyTrip>>(null)
 
   useEffect( () => {
     (async ()=>{
-      setListActiveTrips(getActiveTrips())
+      setListActiveTrips(await getActiveTrips())
     })()
-  })
+  },[])
 
 
   if (!listActiveTrips)
     return <Loading />
 
   return (<div>
-    <header>
-      <div>My Trips</div>
+    <header className="p-4">
+      <div className="text-2xl font-semibold">My Trips</div>
     </header>
+
+    {listActiveTrips.map( t => <div key={t.id}>
+      <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={<img alt="photo" src={t.program.photoGallery[0]} />}
+    >
+      <Meta title={t.destination.description} description={t.program.description} />
+    </Card>,
+    </div>)}
   </div>)
 }
--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,20 @@ type User = {
   lastName: string,
   avatar: string
 }
+
+type MyTrip = {
+  id: string,
+  destination: {
+    id: string,
+    description: string,
+  }, 
+  program: {
+    id: string,
+    description: string,
+    photoGallery: Array<string>
+  },
+  dates: {
+    from: number,
+    to: number
+  }
+}
